be76aab29b9a97cfaa46d3d4aadb2bb9b79a9356
JacobBanda <bandaj2@icloud.com>
2024-01-12 21:23:40 -0600

    update javascript - currentYear

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,4 +1,7 @@
 function lastUpdated() {
+    YearElement.textContent = ` ${currentYear} Your Website`;
+    document.getElementById("currentyear").innerHTML = YearElement;
+   
     let a = document.lastModified;
     document.getElementById("lastmodified").innerHTML = a;
 }

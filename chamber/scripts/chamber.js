b888bf7231f0a70dadfbca1e994d56aba09c7bf4
JacobBanda <bandaj2@icloud.com>
2024-01-15 16:38:01 -0600

    Chamber Week2

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,10 @@
+function lastUpdated() {
+    let currentYear = new Date().getFullYear();
+    document.getElementById("currentyear").textContent = currentYear;
+
+    let lastModified = document.lastModified;
+    document.getElementById("lastmodified").textContent = `Last Modified: ${lastModified}`;
+}
+
+
+lastUpdated();

7eded63e269497caf403a40c2ed9f22ab6470852
JacobBanda <bandaj2@icloud.com>
2024-01-15 18:13:36 -0600

    New modifications - Chamber

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,12 @@
+function toggleMenu() {
+    document.getElementById('primaryNav').classList.toggle('open');
+    document.getElementById('hamburguerBtn').classList.toggle('open');
+}
+
+const x = document.getElementById('hamburguerBtn');
+
+x.onclick = toggleMenu;
+
 function lastUpdated() {
     let currentYear = new Date().getFullYear();
     document.getElementById("currentyear").textContent = currentYear;
@@ -8,3 +17,11 @@ function lastUpdated() {
 
 
 lastUpdated();
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});

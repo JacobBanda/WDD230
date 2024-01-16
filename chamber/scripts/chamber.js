55bed743f19cee6b433196de27cfaf121f1e6444
JacobBanda <bandaj2@icloud.com>
2024-01-15 18:50:35 -0600

    fix nav

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,3 +1,5 @@
+lastUpdated();
+
 function toggleMenu() {
     document.getElementById('primaryNav').classList.toggle('open');
     document.getElementById('hamburguerBtn').classList.toggle('open');
@@ -8,20 +10,7 @@ const x = document.getElementById('hamburguerBtn');
 x.onclick = toggleMenu;
 
 function lastUpdated() {
-    let currentYear = new Date().getFullYear();
-    document.getElementById("currentyear").textContent = currentYear;
 
     let lastModified = document.lastModified;
     document.getElementById("lastmodified").textContent = `Last Modified: ${lastModified}`;
 }
-
-
-lastUpdated();
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
